d12af87c4ae710abc39d7bb6c3b5c8174ebfabe1
ZanW <ZanW@users.noreply.github.com>
2016-12-26 19:16:08 -0500

    Add files via upload

--- merge excel.py
+++ merge excel.py
@@ -1,4 +1,4 @@
-
+rowValue = []
 all_excelfiles = ["","",""]
 all_InOneExcel = [""]
 
@@ -18,6 +18,21 @@ def getSheets(fh):
 # read the numbers of row in one sheet
 def getRowNum(fh, sheetpage):
 	sheetList = fh.sheets()
-	table = sheetlist
+	table = sheetList[sheetpage]
+	sheetRNum = table.nrows
+	return sheetRNum
 	
+# read a file and get all values of its each row
+# row_values() returns a slice of the values of the cells in the given row
+# sheet_by_name returns an object of the Sheet class
+def getFileValue(file, sheetpage):
+		fh = open.xls(file)
+		sheetList = fh.sheets()
+		list_sheetName = fh.sheet_by_name(sheetList[sheetpage])
+		sheetRNum = getRowNum(fh, sheetpage)
+		for RNum in range(0, sheetRNum):
+			 RData = list_sheetName.row_values(RNum)
+			 rowValue.append(RData)
+	
+# read the first expecting file and 	
 	
