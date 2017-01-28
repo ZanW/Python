5e8d98c466fb86b8401d58b0bbb93f9b477ea924
ZanW <ZanW@users.noreply.github.com>
2017-01-28 15:26:38 -0500

    Add files via upload

--- get_comments_of_videoTecent.py
+++ get_comments_of_videoTecent.py
@@ -10,23 +10,28 @@ opener.addheaders = [headers]
 urllib.request.install_opener(opener)
 
 
-commentid = "6228219336534345421"
-url = "http://coral.qq.com/article/1674900933/comment?commentid=" + commentid + "&reqnum=20&tag=&callback=jQuery112003986352910587363_1485136507216&_=1485136507223"
+commentid = "6215151302735378810"
+# url = "http://coral.qq.com/article/1674900933/comment?commentid=" + commentid + "&reqnum=20&tag=&callback=jQuery112003986352910587363_1485136507216&_=1485136507223"
+url = "http://coral.qq.com/article/1658561340/comment?commentid=" + commentid + "&reqnum=20&tag=&callback=jQuery112003926065771866044_1485272723919&_=1485272723926"
 print(url)
 
-for i in range(0, 5):
+for i in range(0, 3):
     try:
         data = urllib.request.urlopen(url).read().decode()
     except Exception as e:
         print(e)
+    #print(data)
+
     patnextpage = '"last":"(.*?)",'
-    allnextpage = re.compile(patnextpage).findall(data)[0]
+    allnextpage = re.compile(patnextpage).findall(data)
     print(allnextpage)
+
     patcomment = '"content":"(.*?)",'
     allcomments = re.compile(patcomment).findall(data)
     print(allcomments)
     for j in range(0, len(allcomments)):
         print("------page "+str(i)+" comment "+ str(j)+" is:" + "------")
         print(eval('u"'+allcomments[j]+'"'))
-    url =  "https://coral.qq.com/article/1674900933/comment?commentid=" + allnextpage + "&reqnum=20&tag=&callback=jQuery112003986352910587363_1485136507216&_=1485136507223"
+    url =  "http://coral.qq.com/article/1658561340/comment?commentid=" + allnextpage[0] + "&reqnum=20&tag=&callback=jQuery112003926065771866044_1485272723919&_=1485272723926"
     print(url)
+
