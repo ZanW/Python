0f3d359678904c0daca9e40ec9be001969259c0e
ZanW <ZanW@users.noreply.github.com>
2017-03-06 09:04:28 -0500

    Add files via upload

---
+++ plot_test.py
@@ -0,0 +1,22 @@
+import matplotlib.pylab as pyl
+import numpy as npy
+x = [1,2,3,4,8]
+y = [5,7,2,1,5]
+#pyl.plot(x, y)
+#pyl.show()
+#pyl.plot(x, y, "o")
+#pyl.show()
+'''
+pyl.plot(x, y)
+pyl.title("show")
+pyl.xlabel("ages")
+pyl.ylabel("temp")
+pyl.xlim(0,10)
+pyl.ylim(0,8)
+pyl.show()
+'''
+import numpy as npy
+data = npy.random.random_integers(1, 20, 10)
+print(data)
+data2 = npy.random.normal(5, 2, 10)
+print(data2)
