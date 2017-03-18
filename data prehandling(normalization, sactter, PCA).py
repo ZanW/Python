1b6d7c4605aeec90e7f2a7554e52ebca2dbcb963
ZanW <ZanW@users.noreply.github.com>
2017-03-18 11:30:25 -0400

    Add files via upload

--- data prehandling(normalization, sactter, PCA).py
+++ data prehandling(normalization, sactter, PCA).py
@@ -2,27 +2,60 @@
 import pandas as pda
 import pymysql
 import numpy as npy
+import matplotlib.pylab as mpl
 
 conn = pymysql.connect(host = "127.0.0.1", user = "root", passwd = "root", db = "test", charset = "utf8")
-sql = "select * from taob"
+sql = "select * from jd"
 data = pda.read_sql(sql, conn)
-'''
-data1 = data
+#convert string to number
+#file = "C:/Users/Administrator/Desktop/jd.xls"
+#data.to_excel(file, index = False)
+
+data1 = pda.read_excel("C:/Users/Administrator/Desktop/jd.xls")
+print(data1.describe())
+
 for i in range(len(data1["price"])):
-    if data1["price"][i].isdigit():
-        print()
-    else:
-        int(data1["price"][i])
+    if data1["price"][i] == 0:
+        data1["price"][i] = 767
 
-print(data.describe())
-'''           
-               
-               
+#convert dataframe to arry
+data2 = data1.T
+price = data2.values[3]
+rate = data2.values[4]
+mpl.plot(price, rate, "o")
+mpl.show()
 
+# if price greater than 3900, comments greater than 95, considered as abnormal values
+for j in range(len(price)):
+    if price[j] > 3900:
+        price[j] = 3900
+    if rate[j] > 95:
+        rate[j] = 95
 
-#print(data)
-#print(data - data.min())
+# plot need array not dataframe
+mpl.plot(price, rate, "o")
+mpl.show()
+
+# step analysis
+price_range = (price.max()- price.min())/12
+rate_range = (rate.max()-rate.min())/12
+
+pricesty = npy.arange(price.min(),price.max(),price_range)
+mpl.hist(price, pricesty)
+mpl.show()
+
+ratesty = npy.arange(rate.min(),rate.max(), rate_range)
+mpl.hist(rate, ratesty)
+mpl.show()
+
 # Normalize Data
-#(1)min-max normalization
-#data2 =(data - data.min())/(data.max()-data.min())
-#print(data2)
+# choose price and rate, exclude title, link typed string
+sql = "select price,rate from jd"
+dat = pda.read_sql(sql, conn)
+
+file = "C:/Users/Administrator/Desktop/jd(price&rate).xls"
+dat.to_excel(file, index = False)
+dat1 = pda.read_excel(file)
+
+#(1)min-max normalization(use dataframework directly)
+dat2 = (dat1-dat1.min())/(dat1.max()-dat1.min())
