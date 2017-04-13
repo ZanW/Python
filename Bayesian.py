6562ddbf2f8dd897ddaf19c73806b35a1992eb37
ZanW <ZanW@users.noreply.github.com>
2017-04-13 10:17:10 -0400

    Add files via upload

---
+++ Bayesian.py
@@ -0,0 +1,11 @@
+class Bayes:
+    def __init__(self):
+        self.length = -1
+        self.label = dict()
+        self.vector = dict()
+    def fit(self, dataSet:list, labels:list):
+        if(len(dataSet) != len(labels)):
+            raise ValueError("test dataset and labels have different length")
+        self.length = len(dataSet[0]) # the length of feature data in test dataset
+        labelsum = len(labels) # total number of labels
+        noReLabels = 
