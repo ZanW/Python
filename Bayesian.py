1b994d1625601a3e3063216ec5f0c132d1577204
ZanW <ZanW@users.noreply.github.com>
2017-04-14 10:10:08 -0400

    Add files via upload

--- Bayesian.py
+++ Bayesian.py
@@ -1,6 +1,6 @@
 class Bayes:
     def __init__(self):
-        self.length = -1
+        self.length = -1 # -1 indicates data has not trained yet
         self.label = dict()
         self.vector = dict()
     def fit(self, dataSet:list, labels:list):
