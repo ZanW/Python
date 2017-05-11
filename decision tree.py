efd4fb64e89e9038d6c8c22c36e8b92b9be97061
ZanW <ZanW@users.noreply.github.com>
2017-05-11 09:12:57 -0400

    Add files via upload

--- decision tree.py
+++ decision tree.py
@@ -18,7 +18,24 @@ for i in range(0, len(y)):
         y[i] = -1
 # fallible point: train x and y directly here
 # right way: convert data type of x and y from object to datframe, then to array designated with type
+xf = pda.DataFrame(x)
+yf = pda.DataFrame(y)
+x2 = xf.as_matrix().astype(int)
+y2 = yf.as_matrix().astype(int)
 
 #build decision tree
 from sklearn.tree import DecisionTreeClassifier as DTC
 dtc = DTC(criterion = "entropy")
+dtc.fit(x2, y2)
+
+#predict sales volume
+import numpy as npy
+x3 = npy.array([[1, -1, -1, 1]])
+result = dtc.predict(x3)
+print(result)
+
+# visualize decision tree
+from sklearn.tree import export_graphviz
+from sklearn.externals.six import StringIO
+with open("C:/Users/Administrator/Desktop/dtc.dot", "w") as file:
+    export_graphviz(dtc, feature_names = ["Practice", "Course Num", "Sales Promotion", "datum"], out_file = file)
