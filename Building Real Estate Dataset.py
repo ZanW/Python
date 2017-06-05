c8669c0faad4df79b8e252cd539af8c8aa086c14
ZanW <ZanW@users.noreply.github.com>
2017-06-05 09:25:48 -0400

    Add files via upload

--- Building Real Estate Dataset.py
+++ Building Real Estate Dataset.py
@@ -1,5 +1,10 @@
 import quandl
 import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use("ggplot")
+
 
 # api_key = open
 
@@ -22,5 +27,14 @@ for code in codeNum:
         #main_df = pd.merge(main_df, df, on = "Date", how = "outer")
         main_df = pd.concat([main_df, df])
 
-print(main_df.head())
-type(main_df)
+pickle_out = open("C:\\Users\\Administrator\\Desktop\\fiddy_states.pickle", "wb")
+pickle.dump(main_df, pickle_out)
+pickle_out.close()
+
+pickle_in = open("C:\\Users\\Administrator\\Desktop\\fiddy_states.pickle", "rb")
+HPI_data = pickle.load(pickle_in)
+print(HPI_data)
+
+HPI_data.plot()
+plt.legend().remove()
+plt.show() 
