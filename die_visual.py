bafff76542d316427b78f9e53e13fc7e438ee263
ZanW <ZanW@users.noreply.github.com>
2017-06-21 11:52:52 -0400

    Add files via upload

--- die_visual.py
+++ die_visual.py
@@ -1,4 +1,5 @@
 from die import Die
+import pygal
 
 '''Create a six_sided die.'''
 die = Die()
@@ -12,8 +13,19 @@ for roll_num in range(1000):
 print(results)
 
 frequencies = []
-for value in range(1, die.num_sides):
+for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 print(frequencies)
+
+
+'''visualize the results.'''
+hist = pygal.Bar()
+hist.title = ""
+hist.x_labels = ["1","2","3","4","5","6"]
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add("D6", frequencies)
+hist.render_to_file("C:\\Users\\Asymmetry\\Desktop\\die_visual.svg")
