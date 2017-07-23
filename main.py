8c0d958088e019b43461c917c0f4248e57dfe5c7
ZanW <ZanW@users.noreply.github.com>
2017-07-23 10:58:06 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,76 @@
+import os
+import zipfile
+import pandas as pd
+
+
+class main():
+    '''
+    def __init__(cls):
+        cls.dataset_path = "C:\\Users\\Asymmetry\\Desktop\\dataset\\"
+        cls.zip_path = "loan.csv.zip"
+        cls.csv_path = "loan.csv"
+    '''
+
+    dataset_path = "C:\\Users\\Asymmetry\\Desktop\\dataset\\"
+    zip_path = "loan.csv.zip"
+    csv_path = "loan.csv"
+
+    # main function
+    @classmethod
+    def run_main(cls):
+        zip_file_path = os.path.join(cls.dataset_path, cls.zip_path)
+        csv_file_path = os.path.join(cls.dataset_path, cls.csv_path)
+
+        # if CSV file is not existing, extract zip file
+        if not os.path.exists(csv_file_path):
+            with zipfile.ZipFile(zip_file_path) as zf:
+                zf.extract(cls.dataset_path)
+
+        # read file
+        raw_data = pd.read_csv(csv_file_path)
+        # go through dataset
+        cls.inspect_data(raw_data)
+
+        # boolean mask as filter
+        # isin(): Return a boolean Series showing whether each element in the Series is exactly contained in the passed sequence of values.
+        filter_mask = raw_data["loan_status"].isin(["Fully Paid","Charged Off","Default"])
+        filter_data = raw_data[filter_mask]
+        print(filter_data["loan_status"].value_counts())
+
+        # alter status "Fully Paid" to 0, otherwise to 1
+        proc_filter_data = filter_data.copy()
+        proc_filter_data["label"] = filter_data["loan_status"].apply(cls.create_label)
+
+        # alter features of "emp_length"
+        proc_filter_data["emp_length_feat"] = filter_data["emp_length"].apply(cls.proc_emp_length)
+
+    @staticmethod
+    def create_label(status_val):
+        label = 1
+        if status_val == "Fully Paid":
+            label = 0
+        return label
+
+
+    @staticmethod
+    def inspect_data(df_data):
+        print("\ndataset preview")
+        print(df_data.head())
+
+        print("\ndataset statistical information")
+        print(df_data.describe())
+
+        print("\ndataset basic information")
+        print(df_data.info())
+
+    @staticmethod
+    def proc_emp_length(emp_length_val):
+        if emp_length_val.split()[0] == "<" or emp_length_val.split()[0] == "n/a":
+            label = 0.5
+        elif emp_length_val.split()[0] == "10+":
+            label = 10
+        else: label = emp_length_val.split()[0]
+
+        return label
+
+main.run_main()
